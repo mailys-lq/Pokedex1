9b93b9b14a7976f206f6e49f8d54017fb695630b
mailys-lq <mailys.lequintrec@gmail.com>
2021-02-26 14:00:45 +0100

    initialisation de la PWA

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Pokemons :pokemons="[
+    {id: '1', name: 'Bulbizarre'},
+    {id: '2', name: 'Herbizarre'},
+    {id: '3', name: 'Florizarre'},
+    {id: '4', name: 'Salamèche'},
+    {id: '5', name: 'Reptincel'},
+    {id: '6', name: 'Dracaufeu'},
+    {id: '7', name: 'Carapuce'},
+    {id: '8', name: 'Carabaffe'},
+    {id: '9', name: 'Tortank'},
+    {id: '10', name: 'Chenipan'},
+    {id: '11', name: 'Chrysacier'},
+    {id: '12', name: 'Papilusion'},
+    ]"/>
+  </div>
+</template>
+
+<script>
+import Pokemons from './components/Pokemons.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pokemons
+  }
+}
+</script>
+
+<style>
+  @import './app.css';
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pokemon">
+    <img :src='getImgUrl(pokemon.id)' v-bind:alt='pokemon.name' width="100" height="100"/>
+    <p>[{{pokemon.id}}] {{pokemon.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pokemon',
+  props: {
+    pokemon: Object
+  },
+  methods: {
+    getImgUrl(id){
+      var images = require.context('../assets/', false, /\.png$/)
+      return images('./' + id + ".png")
+    }
+  }
+}
+</script>
+
+<style>
+  @import 'pokemon.css';
+</style>
